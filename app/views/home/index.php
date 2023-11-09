fb5f32257d71c64bb07423f894fbd880484ddf66
Yulkaz <yulkazs@outlook.com>
2023-11-09 13:42:19 +0100

    SASS opgezet en global.scss toegevoegd (basis styling voor elk pagina)

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rocambolesque ®</title>
+    <link rel="stylesheet" href="public/css/global.css">
 </head>
 <body>
     <!-- Hier komt de Home pagina -->
-
+    
     <!-- FEATURE BRANCH NAAM: HOME -->
 </body>
 </html>
