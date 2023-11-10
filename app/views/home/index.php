a5306eb5b35bd9ddf24d4ef87c8867c4e02733cd
Shahed Amer <chanyeol6521354@gmail.com>
2023-11-10 13:00:36 +0100

    test

---
+++ app/views/home/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rocambolesque ®</title>
+    <link rel="stylesheet" href="public/css/global.css">
+</head>
+
+<body>
+    <?php
+    $tags = [
+        '<link rel="stylesheet" href="/public/css/index.css">',
+    ];
+    $data = [
+        'tags' => $tags,
+        'nav' => true
+    ];
+
+
+    $this->utils('header', $data);
+
+    ?>
+    <!-- Hier komt de Home pagina -->
+    <!-- FEATURE BRANCH NAAM: HOME -->
+</body>
+
+</html>
