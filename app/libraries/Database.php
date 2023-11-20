188bd5b16a6f3432d3b7693cb16e6d5bd1b5ba1f
Amine1Amine <334491@student.mboutrecht.nl>
2023-11-20 12:25:04 +0100

    Amine comitted scss and css files

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -21,10 +21,7 @@ class Database
         $this->statement = $this->dbHandler->prepare($sql);
     }
 
-    public function bind($parameter, $value)
-    {
-        $this->statement->bindValue($parameter, $value);
-    }
+   
 
 
     public function execute()
