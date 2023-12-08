02635d9c5438e2af79780811c973bd754a32f311
Shahed Amer <chanyeol6521354@gmail.com>
2023-12-08 13:12:03 +0100

    In footer heb ik de link van de contact gelinkt

--- app/utils/footer.php
+++ app/utils/footer.php
@@ -9,7 +9,7 @@
     <div>
         <h1 class="brown-text">Contact</h1>
         <p>+31 06 123456789</p>
-        <p><a href="../views/contact/index.php">Neem Contact op →</a></p>
+        <p><a href="../contact/">Neem Contact op →</a></p>
     </div>
 
     <div class="footer-section">
