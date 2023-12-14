a41736f2a13a9e627a1e4bedde004367accff002
omarutrecht <omarchebab@outlook.com>
2023-12-14 11:27:09 +0100

    db script gemaakt

--- app/views/menukaart/index.php
+++ app/views/menukaart/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,30 +10,31 @@
 
     <title>Rocambolesque ® - Menu</title>
 </head>
+
 <body>
 
-<a href="">Ga Terug</a>
+    <a href="">Ga Terug</a>
 
-<h1 class="menu-titel">MENU</h1><br>
-<br><br>
-<br><br>
+    <h1 class="menu-titel">MENU</h1><br>
+    <br><br>
+    <br><br>
 
-<div>
+    <div>
 
-    <div class="CHEFS-BITES">
-    <h2>CHEFS BITES</h2>
-    <p>Minimaal 2 personen (prijs per persoon)</p> 
-    <p class="minimum">Minimum of 2 persons (price per person)</p> 
-    </div>
-    <br>
-     <br>
+        <div class="CHEFS-BITES">
+            <h2>CHEFS BITES</h2>
+            <p>Minimaal 2 personen (prijs per persoon)</p>
+            <p class="minimum">Minimum of 2 persons (price per person)</p>
+        </div>
+        <br>
+        <br>
 
         <div class="roca-container">
-           <div class="roca">
-            Roca 4 Gangen<br>
-            Roca 6 Gangen<br>
-            Roca 8 Gangen<br>
-          </div>
+            <div class="roca">
+                Roca 4 Gangen<br>
+                Roca 6 Gangen<br>
+                Roca 8 Gangen<br>
+            </div>
 
             <div class="roca-prijs">
                 20,50<br>
@@ -43,44 +45,46 @@
 
         <br><br>
 
-                <div class="diner-kaart">
-                    <h2>DINER KAART</h2>
-                    <div>
-                    <p>Red Dragon Maki <br>
-                        King crab en pittige tonijn <br>
-                        King crab and spicy tuna</p></div><br>
+        <div class="diner-kaart">
+            <h2>DINER KAART</h2>
+            <div>
+                <p>Red Dragon Maki <br>
+                    King crab en pittige tonijn <br>
+                    King crab and spicy tuna</p>
+            </div><br>
 
-                        <p>Unagi Maki<br> 
-                            Paling, komkommer en avocado<br>
-                            Eel, cucumber and avocado</p><br>
+            <p>Unagi Maki<br>   
+                Paling, komkommer en avocado<br>
+                Eel, cucumber and avocado</p><br>
 
-                            <p>Carpaccio Maki <br>
-                                Dungesneden entrecôte en truffelsalsa <br>
-                                Thin sliced sirloin and truffle salsa</p><br>
+            <p>Carpaccio Maki <br>
+                Dungesneden entrecôte en truffelsalsa <br>
+                Thin sliced sirloin and truffle salsa</p><br>
 
-                                <p>Wagyu Maki<br>
-                                    Geflambeerde wagyu beef A5<br>
-                                    Flambéed wagyu beef A5</p><br>
-                </div>
+            <p>Wagyu Maki<br>
+                Geflambeerde wagyu beef A5<br>
+                Flambéed wagyu beef A5</p><br>
+        </div>
 
 
-                    <table>
-                        
-                        <p>(4st/4pcs)</p>              
+        <table>
 
-                           <p>12,00</p>
-                           <p> 13,00</p>
-                           <p> 09,50</p>
-                           <p>14,50</p>
-                    </table>
+            <p>(4st/4pcs)</p>
 
-                                                           <table>
-                                                        <p>(8st/8pcs)</p>
+            <p>12,00</p>
+            <p> 13,00</p>
+            <p> 09,50</p>
+            <p>14,50</p>
+        </table>
 
-                                                            <p>23,00</p>
-                                                            <p>25,00</p>
-                                                            <p>18,50</p>
-                                                            <p>28,00</p>
-                                                            </table>
+        <table>
+            <p>(8st/8pcs)</p>
+
+            <p>23,00</p>
+            <p>25,00</p>
+            <p>18,50</p>
+            <p>28,00</p>
+        </table>
 </body>
+
 </html>
